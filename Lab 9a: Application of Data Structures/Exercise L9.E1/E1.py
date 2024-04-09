83d4550cc28589e8418561e4c9082000c73befa7
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2024-04-09 13:42:02 +0530

    Create E1.py

---
+++ Lab 9a: Application of Data Structures/Exercise L9.E1/E1.py
@@ -0,0 +1,10 @@
+You are required to implement a simple symbolic equation solver. The equation must be stored in a binary tree.
+Each operand or operator should be stored as a tuple of the form (TYPE, VALUE). 
+
+For example: (OPERAND, 5), (OPERAND, 7), (OPERAND, 34), (OPERATOR, ‘+’) or (OPERATOR, '*’').
+
+Following operators should be supported: addition (+), subtraction (-), multiplication (*), and exponentiation (^).
+
+Further details, hints, and examples on the exercise can be found in the lab sheet.
+
+Skeleton of this lab exercise is given below. Complete the bodies of the insert, and evaluate methods. Include your solution in the sections mentioned as "Include your code here". Please do NOT change any of the code outside of these two methods.
